18638edee5a6357383a416aec2ae0a36fb7550e6
AndreyChaki <andreychaki@gmail.com>
2020-08-03 01:31:07 +0500

    final

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,72 @@
 import React from "react"
-import { graphql, StaticQuery } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Course from "../components/Course"
+import SEO from "../components/seo"
+import Img from "gatsby-image"
 
-const Index = () => (
-    <Layout title="siteTitle">
-      <div className="course">
-        <StaticQuery
-          query={graphql`
-          {
-            allContentfulCourse (sort: {fields: slug}) {
-              edges {
-                node {
-                  slug
-                  title
-                  author
-                  image {
-                    fluid (toFormat: WEBP) {
-                      ...GatsbyContentfulFluid
-                    }
-                  }
-                }
-              }
-            }
-          }
-        `}
-          render={({
-            allContentfulCourse: {
-             edges
-            }
-            }) => (
-            edges.map(({ node }) => (
-              <Course key={node.slug} content={node} />
-            ))
-          )}
-        />
+const Index = ({ data }) => {
+  const siteTitle = data.site.siteMetadata.title
+  const courses = data.allContentfulCourse.edges
+
+
+  return (
+    <Layout title={siteTitle}>
+      <SEO title={siteTitle} />
+
+      <div className='course'>
+        {courses.map(({ node }) => {
+          const title = node.title || node.slug
+          return (
+            <article className='courseItem' key={node.slug}>
+              <div>
+                <Link to={node.slug}>
+                  {title}
+                </Link>
+              </div>
+              <div>
+                Автор: {node.author}
+              </div>
+              <div>
+                Количество уроков: {node.child.length}
+              </div>
+              <div>
+                <Img fluid={node.image.fluid} />
+              </div>
+            </article>
+          )
+        })}
       </div>
-    </Layout>
 
-)
+    </Layout>
+  )
+}
 
 export default Index
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allContentfulCourse {
+      edges {
+        node {
+          slug
+          title
+          author
+          image {
+            fluid (toFormat: WEBP) {
+              ...GatsbyContentfulFluid
+            }
+          }
+          child {
+            id
+          }
+        }
+      }
+    }
+  }
+`
